1d4611907ba07e16513aaf87715d265a2e7a6837
Hasaan Azeem <hasaanazeem07@gmail.com>
2025-08-08 12:53:57 +0500

    cv add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,25 +5,35 @@ import React, { useRef, useState } from "react";
 const Home = () => {
   const tiltRef = useRef(null);
 
-
   const [xVal, setXVal] = useState(0);
   const [yVal, setYVal] = useState(0);
 
   const mouseMoving = (e) => {
-
-    setXVal((e.clientX - tiltRef.current.getBoundingClientRect().x - tiltRef.current.getBoundingClientRect().width/2)/50);
-    setYVal(-(e.clientY - tiltRef.current.getBoundingClientRect().y -  tiltRef.current.getBoundingClientRect().height/2)/10);
-
+    setXVal(
+      (e.clientX -
+        tiltRef.current.getBoundingClientRect().x -
+        tiltRef.current.getBoundingClientRect().width / 2) /
+        50
+    );
+    setYVal(
+      -(
+        e.clientY -
+        tiltRef.current.getBoundingClientRect().y -
+        tiltRef.current.getBoundingClientRect().height / 2
+      ) / 10
+    );
   };
 
-  useGSAP(function (){
-    gsap.to(tiltRef.current, {
-      transform : `rotateX(${yVal}deg) rotateY(${xVal}deg)`,
-      duration: 3,
-      ease: "elastic.out(1, 0.3)",
-    })
-  },[xVal, yVal]);
-
+  useGSAP(
+    function () {
+      gsap.to(tiltRef.current, {
+        transform: `rotateX(${yVal}deg) rotateY(${xVal}deg)`,
+        duration: 3,
+        ease: "elastic.out(1, 0.3)",
+      });
+    },
+    [xVal, yVal]
+  );
 
   return (
     <div
@@ -34,42 +44,61 @@ const Home = () => {
       className="relative h-screen w-full bg-white p-2 xs:p-3 sm:p-4 md:p-5"
     >
       <div
-  id="main"
-  className="relative w-full h-full bg-[url('/src/assets/profile.png')] sm:bg-[url('/src/assets/desktop.png')] bg-cover bg-center bg-no-repeat p-4 sm:p-6 md:p-8 rounded-2xl sm:rounded-3xl shadow-lg "
->
-  
-<div className="absolute inset-0 sm:hidden bg-gradient-to-tr from-black/90 to-transparent z-0 rounded-2xl"></div>
-<div className="absolute inset-0 sm:hidden bg-gradient-to-bl from-black/20 to-transparent z-0 rounded-2xl"></div>
+        id="main"
+        className="relative w-full h-full bg-[url('/src/assets/profile.png')] sm:bg-[url('/src/assets/desktop.png')] bg-cover bg-center bg-no-repeat p-4 sm:p-6 md:p-8 rounded-2xl sm:rounded-3xl shadow-lg "
+      >
+        <div className="absolute inset-0 sm:hidden bg-gradient-to-tr from-black/90 to-transparent z-0 rounded-2xl"></div>
+        <div className="absolute inset-0 sm:hidden bg-gradient-to-bl from-black/20 to-transparent z-0 rounded-2xl"></div>
+
+        <div
+          id="tiltDiv"
+          ref={tiltRef}
+          className="relative z-10 text-left mt-80 xl:mt-30 2xl:mt-60 py-2 sm:py-5 px-3"
+        >
+          <h1 className="text-3xl sm:text-3xl md:text-4xl lg:text-xl xl:text-5xl 2xl:text-8xl uppercase text-black font-[apna] font-extrabold leading-tight">
+            I am <span className="text-[#08aaaa]">DARK MODE</span>
+            <span className="font-[apna3]">™</span>
+          </h1>
+
+          <h1 className="text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-black uppercase font-[apna] font-extrabold leading-tight tracking-tight sm:tracking-wide mt-2">
+            Website Developer
+          </h1>
+
+          <h1 className="text-[#08aaaa] text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-7xl uppercase font-[apna] font-extrabold leading-tight mt-2">
+            To Hire
+          </h1>
+        </div>
+
+        <div className="relative ml-5 mt-10 z-10">
+          <a
+            href="/Hasaan_Azeem.pdf"
+            download
+            className="px-2 py-2 sm:px-3 sm:py-3 font-semibold rounded-lg shadow-md border-2 border-[#08aaaa] text-white sm:text-[#08aaaa] transition duration-300 ease-in-out transform hover:scale-105 hover:bg-[#08aaaa] hover:text-white"
 
 
+          >
+            Download My CV
+          </a>
+          <a
+            onClick={() => {
+                const el = document.getElementById("contact");
+                if (el) el.scrollIntoView({ behavior: "smooth" });
+              }}
+            download
+            className="ml-2 sm:ml-5 px-2 py-2 sm:px-3 sm:py-3 font-semibold rounded-lg shadow-md border-2 border-[#08aaaa] text-white sm:text-[#08aaaa] transition duration-300 ease-in-out transform hover:scale-105 hover:bg-[#08aaaa] hover:text-white"
 
+          >
+            Contact Me
+          </a>
+        </div>
 
-  <div
-    id="tiltDiv"
-    ref={tiltRef}
-    className="relative z-10 text-left mt-80 xl:mt-30 2xl:mt-60 py-2 sm:py-5 px-3"
-  >
-    <h1 className="text-3xl sm:text-3xl md:text-4xl lg:text-xl xl:text-5xl 2xl:text-8xl uppercase text-black font-[apna] font-extrabold leading-tight">
-      I am <span className="text-[#08aaaa]">DARK MODE</span>
-      <span className="font-[apna3]">™</span>
-    </h1>
-
-    <h1 className="text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-black uppercase font-[apna] font-extrabold leading-tight tracking-tight sm:tracking-wide mt-2">
-      Website Developer
-    </h1>
-
-    <h1 className="text-[#08aaaa] text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-7xl uppercase font-[apna] font-extrabold leading-tight mt-2">
-      To Hire
-    </h1>
-  </div>
-
-  {/* Footer Text */}
-  <div className="absolute bottom-4 sm:bottom-30 md:bottom-10 left-8 sm:left-6 md:left-8 lg:left-14 z-10">
-    <h2 className="text-white sm:text-black font-[apna3] font-semibold tracking-wider text-md sm:text-lg md:text-xl uppercase">
-      Full stack developer
-    </h2>
-  </div>
-</div>
+        {/* Footer Text */}
+        <div className="absolute bottom-4 sm:bottom-30 md:bottom-5 left-8 sm:left-6 md:left-8 lg:left-14 z-10">
+          <h2 className="text-white sm:text-black font-[apna3] font-semibold tracking-wider text-md sm:text-lg md:text-xl uppercase">
+            Full stack developer
+          </h2>
+        </div>
+      </div>
     </div>
   );
 };
